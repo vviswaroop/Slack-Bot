f3cc0b72fc6ebdf1c2f08fb37e26ae5bf5bafb83
Viswaroop Vadlamudi <viswaroopvadlamudi@Viswaroops-MacBook-Pro.local>
2018-10-18 22:54:01 -0700

    adding initializing script

---
+++ startbot.py
@@ -0,0 +1,15 @@
+import os
+import time
+import re
+from slackclient import slack client
+
+#instantiate slack client
+slack client = =slackclient(os.version.get('xoxb-457910190834-460826906759-JSQJlD6UngIrAwJncbGKw21J'))
+
+$starteerbot's user ID in slack :value is assigned after the bot startes up
+starterbot_id = None
+
+#constants
+RTM_READ_DELAY = 1 #1 second delay from reading RTM
+EXAMPLE COMMAND = 'do'
+MENTION_REGEX = "^<0(|[WU].+?)>(.*)"
